264809f1cb4a358f41113f01cf799b55fb3b60b9
Peixotim <pedropeixotovz@gmail.com>
2025-10-29 11:19:30 -0300

    Feat: Adiciona a dependencia do swagger

--- src/pokemon/pokemon.controller.ts
+++ src/pokemon/pokemon.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
+import { PokemonEntity } from './entity/pokemon.entity';
+import { CreatePokemonDto } from './dto/pokemon.create';
+import { PokemonService } from './pokemon.service';
 
 @Controller('pokemon')
-export class PokemonController {}
+export class PokemonController {
+  constructor(private readonly pokemonService: PokemonService) {}
+
+  @Post()
+  @HttpCode(HttpStatus.CREATED)
+  public async createPokemon(
+    @Body() request: CreatePokemonDto,
+  ): Promise<PokemonEntity> {
+    return this.pokemonService.createPokemon(request);
+  }
+}
