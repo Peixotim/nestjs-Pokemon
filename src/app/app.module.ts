d203149bc31efb95502e6bc4bb21064587dad9c5
Peixotim <pedropeixotovz@gmail.com>
2025-10-29 13:59:09 -0300

    Feat: Features

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,7 +13,7 @@ import { PokemonModule } from 'src/pokemon/pokemon.module';
         type: 'postgres',
         host: config.get<string>('DB_HOST'),
         port: Number(config.get<string>('DB_PORT')) || 5432,
-        username: config.get<string>('DB_USERNAME'),
+        username: process.env.DB_USERNAME,
         password: config.get<string>('DB_PASSWORD'),
         database: config.get<string>('DB_NAME'),
         autoLoadEntities: true,
