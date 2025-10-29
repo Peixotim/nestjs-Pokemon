5d10002e45eff29ae6c11d01f3e7d90c9a8c1248
Peixotim <pedropeixotovz@gmail.com>
2025-10-29 11:17:17 -0300

    Feat: Adiciona novas dependencias e configura o TypeORM

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { PokemonModule } from 'src/pokemon/pokemon.module';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      host: 'localhost',
+      port: 5434,
+      database: 'pokemon-db',
+      password: 'ad',
+      username: 'ad',
+      type: 'postgres',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+    PokemonModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, //remove chaves que nao estao no DTO
+      forbidNonWhitelisted: true, //Retorna oque aconteceu
+    }),
+  );
+  await app.listen(process.env.PORT ?? 9999);
 }
 bootstrap();
