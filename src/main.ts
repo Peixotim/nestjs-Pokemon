853015ba8d7136a44f057398d88518007cb461d0
Peixotim <pedropeixotovz@gmail.com>
2025-10-29 14:01:17 -0300

    Feat: Altera a porta da aplicação

--- src/main.ts
+++ src/main.ts
@@ -22,6 +22,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, configSwagger);
   SwaggerModule.setup('api-docs', app, document);
 
-  await app.listen(process.env.PORT ?? 9999);
+  await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
